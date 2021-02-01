9b6ef3ec914d73f1b60c3b7d55292cfc6c87b53f
chendaye666 <chendaye666@gmail.com>
2021-02-01 09:24:09 +0000

    element ui

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
  * Created by chendaye666 on 17/8/5.
  */
 import Vue from 'vue'
-
+import ElementUI from 'element-ui';
 import {
 	sync
 } from 'vuex-router-sync'
@@ -10,7 +10,7 @@ import App from './App.vue'
 import router from './router'
 
 window.jQuery = window.$ = require('jquery/dist/jquery')
-
+Vue.use(ElementUI);
 const app = new Vue({
 	router,
 	...App
